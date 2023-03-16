a45e4a60bd63a58c5e77b6f303633e6957573822
neagualexa <ngu.alexandra@gmail.com>
2023-03-16 13:11:24 +0000

    added HAL_TIM_PeriodElapsedCallback timer6

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1448,6 +1448,15 @@ void CAN_Transmit(void *argument)
   */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
+	/* USER CODE BEGIN Callback 0 */
+
+	/* USER CODE END Callback 0 */
+	if (htim->Instance == TIM16) {
+		HAL_IncTick();
+	}
+	/* USER CODE BEGIN Callback 1 */
+
+	/* USER CODE END Callback 1 */
 }
 
 /**
